1a65f57ad0ce311c67e1b3c10ac7e3452599be4e
assasinfil <nfa57643@gmail.com>
2020-11-01 22:05:00 +0300

    Added graph todo
    Signed-off-by: assasinfil <nfa57643@gmail.com>

--- graph.cpp
+++ graph.cpp
@@ -4,12 +4,15 @@
 
 #include "graph.h"
 
+//TODO add parameters
 Graph::Graph() {}
 
+//TODO add destruction
 Graph::~Graph() {
 
 }
 
+//TODO complete format output
 std::ostream &operator<<(std::ostream &os, const Graph &graph) {
     return os;
 }
