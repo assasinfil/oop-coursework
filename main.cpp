8ed82296a4af3fa6364f75ccfd2e2b6649c8d3df
assasinfil <nfa57643@gmail.com>
2020-11-16 21:42:19 +0300

    Added graph input from file/console
    Signed-off-by: assasinfil <nfa57643@gmail.com>

--- main.cpp
+++ main.cpp
@@ -2,12 +2,17 @@
 // Created by assasinfil on 01.11.2020.
 //
 #include <iostream>
+#include <fstream>
 #include "graph.h"
 
 using namespace std;
 
 int main() {
-    Graph graph(5,4);
+    ifstream f("input.txt");
+    int x, y;
+    f >> x >> y;
+    Graph graph(y, x);
+    f >> graph;
     cout << graph;
     return 0;
 }
